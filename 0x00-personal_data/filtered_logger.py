e3fe6b366eeb6f59cab365c44d772c9dbde04132
deking254 <dekingsky@gmail.com>
2024-01-14 13:25:53 +0000

    this is the code for the first file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""filtering logs"""
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
+    for field in fields:
+        regex = '(?<={}=)[^{}]*'.format(field, separator)
+        message = re.sub(regex, redaction, message)
+    return message
