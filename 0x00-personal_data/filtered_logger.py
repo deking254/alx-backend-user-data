b2efee533853ce361e084ab621c61b501a2db186
deking254 <dekingsky@gmail.com>
2024-01-14 13:29:38 +0000

    this is the code for the first file

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,5 +7,5 @@ def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
     for field in fields:
         regex = '(?<={}=)[^{}]*'.format(field, separator)
-        message = re.sub(regex, redaction, message)
+        re.sub(regex, redaction, message)
     return message
