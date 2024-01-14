3448abb3be0cb8446a91bf7d6fbcac3da5830ad5
deking254 <dekingsky@gmail.com>
2024-01-14 16:20:38 +0000

    fixing annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""filtering logs"""
-from re
+"""filtering logs of personal data"""
+import re
+import logging
 from typing import List
 
 
@@ -8,6 +9,21 @@ def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
-        regex: str = '(?<={}=)[^{}]*'.format(field, separator)
-        message: str = re.sub(regex, redaction, message)
+        regex = '(?<={}=)[^{}]*'.format(field, separator)
+        message = re.sub(regex, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
