739cd140ab8ee586ac6002be179418c9d28dea47
deking254 <dekingsky@gmail.com>
2024-01-14 14:02:38 +0000

    adding annotations

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,7 +4,7 @@ from re
 from typing import List
 
 
-def filter_datum(fields: List,
+def filter_datum(fields: List[str],
                  redaction: str, message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
