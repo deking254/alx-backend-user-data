7fe3be8207e39a2c8dba47930d29ce2787ae2e24
deking254 <dekingsky@gmail.com>
2024-01-14 18:55:33 +0000

    code for logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -24,8 +24,15 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]):
         """this functions is called first when the class is init"""
+        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
         """formats the personal data into a format"""
+        if self.usesTime():
+            record.asctime = self.formatTime(record)
+        record.message = filter_datum(self.fields, self.REDACTION,
+                                      record.getMessage(), self.SEPARATOR)
+        record.message = '; '.join(record.message.split(';'))
+        return self.formatMessage(record)
         NotImplementedError
