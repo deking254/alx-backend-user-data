1e6489af7edc499ebcae99a4067415b1cd6aa39d
deking254 <dekingsky@gmail.com>
2024-01-18 06:03:45 +0000

    code for the require auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """this is the template file for the authenticatins system"""
-import request from flask
+from flask import request
+from typing import List, TypeVar
 
 
 class Auth():
@@ -8,7 +9,18 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns false or true depending on if authenticatin is required"""
-        return False
+        if path is None or path not in excluded_paths:
+            if path is not None:
+                if path[-1] == '/':
+                    if path[0: -1]  in excluded_paths:
+                        return False
+                else:
+                    path = path + '/'
+                    if path in excluded_paths:
+                        return False
+            return True
+        else:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """returns the authorization header"""
