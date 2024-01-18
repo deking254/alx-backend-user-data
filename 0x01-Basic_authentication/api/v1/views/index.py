b7043383b0ce6fc16336fc9156631e6ff2d86813
deking254 <dekingsky@gmail.com>
2024-01-18 05:28:57 +0000

    mades strict slashes unimportant

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'])
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized_route() -> str:
     """testing the 401 error handler"""
     abort(401)
