9aac17edea64911fba0a622abbf59bda4c9b20c0
deking254 <dekingsky@gmail.com>
2024-01-19 12:42:14 +0000

    added the current_user method in the SessionAuth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """This is the Session Authentication module"""
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 class SessionAuth(Auth):
     """the session authentication class"""
@@ -21,3 +22,10 @@ class SessionAuth(Auth):
         if session_id is not None:
             if type(session_id) == str:
                 return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """returns a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
+
