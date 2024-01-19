741da170b080e67a02dafb23c2fdc52db5203c0f
deking254 <dekingsky@gmail.com>
2024-01-19 11:52:04 +0000

    created the create_session function

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """This is the Session Authentication module"""
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """the session authentication class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is not None:
+            if type(user_id) == str:
+                session_id = str(uuid.uuid4())
+                self.user_id_by_session_id[session_id] = user_id
+                return session_id
+        return None
