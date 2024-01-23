4bfaf28a3e9ac8e38a0e1deb874f5ea5d9e0e2dc
deking254 <dekingsky@gmail.com>
2024-01-23 12:12:37 +0000

    added varchar

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """create a SQLAlchemy model named User for a database table named users"""
-from sqlalchemy import (Column, INT, TEXT, String, MetaData)
+from sqlalchemy import (Column, INTEGER, VARCHAR)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,8 +8,8 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(INT, primary_key=True, nullable=False)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    id = Column(INTEGER, primary_key=True, nullable=False)
+    email = Column(VARCHAR(250), nullable=False)
+    hashed_password = Column(VARCHAR(250), nullable=False)
+    session_id = Column(VARCHAR(250), nullable=True)
+    reset_token = Column(VARCHAR(250), nullable=True)
