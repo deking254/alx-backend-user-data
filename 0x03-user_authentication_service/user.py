13ce3f8ac2251713e4615d13766a2a18bb9bf799
deking254 <dekingsky@gmail.com>
2024-01-23 05:06:53 +0000

    names to the columns

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 """create a SQLAlchemy model named User for a database table named users"""
-from sqlalchemy import (create_engine, Table, Column, Integer, String, MetaData)
+from sqlalchemy import (Column, Integer, String, MetaData)
 from  sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
-
-engine = create_engine('mysql+mysqldb://root:@localhost/my_db')
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    id = Column('id', Integer, primary_key=True)
+    email = Column('email', String(250), nullable=False)
+    hashed_password = Column('hashed_password', String(250), nullable=False)
+    session_id = Column('session_id', String(250))
+    reset_token = Column('reset_token', String(250))
