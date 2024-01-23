5a4d5f40bbc4771b657e27e7169e4494e461e763
deking254 <dekingsky@gmail.com>
2024-01-23 09:14:58 +0000

    changed the Integer import to INT

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """create a SQLAlchemy model named User for a database table named users"""
-from sqlalchemy import (Column, INTEGER, String, MetaData)
+from sqlalchemy import (Column, INT, String, MetaData)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(INTEGER, primary_key=True)
+    id = Column(INT, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
