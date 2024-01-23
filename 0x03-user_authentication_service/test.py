47b97f151499b23680d04bfa0fc076a6bd97b1c2
deking254 <dekingsky@gmail.com>
2024-01-23 09:01:57 +0000

    fixed pycodestyles issues

---
+++ 0x03-user_authentication_service/test.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+from unittest import TestCase
+print(dir(TestCase))
+class Test(TestCase):
+    pass
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """create a SQLAlchemy model named User for a database table named users"""
 from sqlalchemy import (Column, Integer, String, MetaData)
-from  sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column('id', Integer, primary_key=True)
-    email = Column('email', String(250), nullable=False)
-    hashed_password = Column('hashed_password', String(250), nullable=False)
-    session_id = Column('session_id', String(250))
-    reset_token = Column('reset_token', String(250))
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
