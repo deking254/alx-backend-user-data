4a94aa267bffd08e2b3edc11a34be8ca2015063a
deking254 <dekingsky@gmail.com>
2024-01-23 09:21:12 +0000

    changed the Integer import to INT

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,8 +8,8 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(INT, primary_key=True)
-    email = Column(TEXT(250), nullable=False)
+    id = Column(TEXT, primary_key=True)
+    email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
     reset_token = Column(String(250))
