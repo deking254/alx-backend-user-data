2b56702ac2dfd1099b7bd673305e4beec2f318da
deking254 <dekingsky@gmail.com>
2024-01-23 08:11:51 +0000

    added the add_user function

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,7 +29,7 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-
+    @classmethod
     def add_user(self, email: str, hashed_password: str) -> User:
         """adds a user to the database
         """
