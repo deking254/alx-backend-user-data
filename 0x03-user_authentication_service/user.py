124d70798ec47e177dcb6f1c2280d27478b6af70
deking254 <dekingsky@gmail.com>
2024-01-23 10:57:16 +0000

    some changes

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -8,6 +8,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
+    id = Column(INT, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250))
