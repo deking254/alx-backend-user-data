815d292f5aba71631cddf02eca3a2fbb49383a46
deking254 <dekingsky@gmail.com>
2024-01-25 12:32:44 +0000

    removed the echo=True

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """create a SQLAlchemy model named User for a database table named users"""
-from sqlalchemy import (Column, INTEGER, VARCHAR)
+from sqlalchemy import (Column, INTEGER, String)
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -10,7 +10,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'users'
     id = Column(INTEGER, primary_key=True, nullable=False)
-    email = Column(VARCHAR(250), nullable=False)
-    hashed_password = Column(VARCHAR(250), nullable=False)
-    session_id = Column(VARCHAR(250), nullable=True)
-    reset_token = Column(VARCHAR(250), nullable=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
